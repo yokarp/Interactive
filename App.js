9c3c8f3fc572f8d5ef85dec1c1e7e2157eb6f5b3
Yokar <yokar@protonmail.com>
2020-09-29 03:43:22 -0500

    Use of camera

--- App.js
+++ App.js
@@ -1,44 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Dimensions} from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-import Constants from 'expo-constants';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
-    const [locacion, setLocacion] = useState({})
+    const [permisos, setPermisos] = useState(null)
+    const [tipo, setTipo] = useState(Camera.Constants.Type.back)
 
-    const buscaLocation = async () => {
-      const { status } = await Location.requestPermissionsAsync()
-      if (status !== 'granted') {
-        return Alert.alert('no tenemos los permisos para la localización')
-      }
+    const getPermisos = async () => {
+      const { status } = await Camera.requestPermissionsAsync()
+      setPermisos( status == 'granted')
       const location = await Location.getCurrentPositionAsync({})
       setLocacion(location)
     }
     useEffect(() =>{
-      buscaLocation()
+      getPermisos()
     })
+
+    if (permisos === null ){
+      return <View><Text>Esperando permisos...</Text></View>
+    }
+    if ( permisos === false ){
+      return <View><Text>No tenemos acceso a la camara :(</Text></View>
+    }
 
     return (
       <View style = { styles.container }>
-        <MapView style={ styles.map }>
-          {locacion.coords
-            ? <Marker
-                coordinate={locacion.coords}
-                title="Tiitulo"
-                description="Descripción del punto" />
-                : null
-          }
-          </MapView>
+        <Camera style={styles.camera} type={tipo}>
+          <Button
+            title="Voltear"
+            onPress={() => {
+                const { front, back } = Camera.Constants.Type
+                const nuevoTipo = tipo === back ? front : back
+                setTipo(nuevoTipo)
+             }
+            }
+          />
+        </Camera>
       </View>
     );
 }
 
 const styles = StyleSheet.create({
-    map:{
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
+    camera:{
+      flex: 1,
     },
     container: {
         flex: 1,
